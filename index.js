dff32b6ee55410f34bf2279d6e06b87cc0369512
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-12 08:33:57 +0300

    cmpleted the array assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+// Write your solution here!np
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('liz');
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('ju');
+}
+
+
+function appendCat(){
+    const newCats = [...cats, "Broom"]
+    return newCats;
+}
+
+function prependCat(){
+    const newCats = ["Arnold", ...cats]
+    return newCats;
+}
+
+
+
+
+
+
+function removeLastCat(){
+    const newCats= [...cats.slice(0,2)];
+    return newCats;
+}
+
+
+function removeFirstCat(){
+    const newCats = [...cats.slice(1)];
+    return newCats;
+}
